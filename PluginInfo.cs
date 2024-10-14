8d1d942565a786f7e3c726ac08453e67bf32810d
Cat-Gorilla-Tag <bigsierzeg@gmail.com>
2024-10-13 20:21:08 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.void.lols.moonpanel";
+        public const string Name = "Moon Panel";
+        public const string Description = "Made by bro12342968 and Catツ";
+        public const string Version = "1.0.0";
+    }
+}
