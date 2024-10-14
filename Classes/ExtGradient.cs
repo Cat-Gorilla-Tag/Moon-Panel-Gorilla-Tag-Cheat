7e1aa0da77d3c4c0e7f5381b1050241ae54e1c0e
Cat-Gorilla-Tag <bigsierzeg@gmail.com>
2024-10-13 20:44:01 -0400

    Add files via upload

---
+++ Classes/ExtGradient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace StupidTemplate.Classes
+{
+    public class ExtGradient
+    {
+        public static Color MenuColor = new Color32(62, 122, 157, 255);
+        public GradientColorKey[] colors = new GradientColorKey[]
+        {
+            new GradientColorKey(Color.black, 0f),
+            new GradientColorKey(MenuColor, 0.5f),
+            new GradientColorKey(Color.black, 1f)
+        };
+
+        public bool isRainbow = false;
+        public bool copyRigColors = false;
+    }
+}
